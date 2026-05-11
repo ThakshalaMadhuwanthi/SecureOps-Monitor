8cbd8a531e35c94927a46228ef28218f32b1c1d2
ThakshalaMadhuwanthi <hgtmadhuwanthi@gmail.com>
2026-05-11 10:46:22 +0530

    fix some issues

--- front-end/script.js
+++ front-end/script.js
@@ -1,5 +1,5 @@
 function checkStatus() {
-    fetch("http://backend:5000/api/status")
+    fetch("http://localhost:5000/api/status")
         .then(res => res.json())
         .then(data => {
             document.getElementById("status").innerText = data.message;
--- frontend.dockerfile
+++ frontend.dockerfile
@@ -1,6 +1,5 @@
 FROM nginx:alpine
 
 
-COPY front-end/. /usr/share/html/
+COPY front-end/  /usr/share/html/
 
-EXPOSE 80
